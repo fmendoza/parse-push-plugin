28d8f954b8f1c96083d3b68f61dcdedbe66c6a00
fmendoza <f.mendoza.galvez@gmail.com>
2019-07-05 20:27:01 -0500

    fix import

--- scripts/afterAndroidPrepare.js
+++ scripts/afterAndroidPrepare.js
@@ -1,7 +1,7 @@
 module.exports = function (context) {
   var fs = require('fs');
   var path = require('path');
-  var ET = context.requireCordovaModule('elementtree');
+  var ET = require('elementtree');
   var ConfigFile = context.requireCordovaModule("cordova-common").ConfigFile;
 
   var configXml = new ConfigFile(context.opts.projectRoot, null, './config.xml');
